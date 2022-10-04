0fd140d7c01abd9fdf5cfef36befd2316a37a4a1
bcheong10 <bcheong10@gmail.com>
2022-10-05 00:09:10 +0800

    New Lesson

---
+++ Learn_C++/hello_world.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Hello World"<< endl; // "endl" ends the line and starts the next texts on a new line
+    std::cout <<"Another way to print"<< endl; // Can include std::cout instead
+    cout <<"    /|"<< endl;
+    cout <<"   / |"<< endl;
+    cout <<"  /  |"<< endl;
+    cout <<" /   |"<< endl;
+    cout <<"/____|"<< endl;
+    cout <<"This is a triangle"<< endl;
+
+
+    return 0;
+}
---
+++ Learn_C++/variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Ben";
+    int age = 23;
+
+    cout <<"Hello, my name is "<< name << endl;
+    cout <<"I am "<< age <<" years old" << endl;
+
+    return 0;
+}
