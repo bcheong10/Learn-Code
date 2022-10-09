5febaf605d083d021ac280eba7058ff4d6f0afdd
bcheong10 <bcheong10@gmail.com>
2022-10-09 19:29:15 +0800

    edits

--- Learn_C++/20_classes_objects.cpp
+++ Learn_C++/20_classes_objects.cpp
@@ -2,7 +2,29 @@
 
 using namespace std;
 
+// Creating a class
+class Book
+{
+    // Listing all attributes of the class
+    public:
+        string title;
+        string author;
+        int pages;
+};
+
 int main()
 {
-    
+    Book book1;
+    book1.title = "Harry Potter";
+    book1.author = "JK Rowling";
+    book1.pages = 500;
+
+    Book book2;
+    book2.title = "Lord of the Rings";
+    book2.author = "Tolkein";
+    book2.pages = 700;
+
+    cout << book1.title << endl;
+    cout << book2.title << endl;
+
 }
