e602e1b4209855fc23a0a3e2add86a3e944c88d7
bcheong10 <bcheong10@gmail.com>
2022-10-19 16:54:22 +0800

    edit

--- Learn_Python/openCV/11_masking.py
+++ Learn_Python/openCV/11_masking.py
@@ -7,7 +7,7 @@ cv.imshow("Original", img)
 # Masking - focus on certain parts of the image
 
 blank = np.zeros(img.shape[:2], dtype = 'uint8')
-cv.imshow("BLank Image", blank)
+cv.imshow("Blank Image", blank)
 
 circle = cv.circle(blank, (img.shape[1]//2 + 70, img.shape[0]//2), 100, 255, -1)
 cv.imshow("Circle", circle)
