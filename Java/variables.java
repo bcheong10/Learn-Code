b08c13c94251303b110bd1c0a9d71f2afa989f75
bcheong10 <97442848+bcheong10@users.noreply.github.com>
2022-11-16 17:54:18 +0800

    Add files via upload

---
+++ Java/variables.java
@@ -0,0 +1,12 @@
+class variables
+{
+    // String name is a global variable
+    static String name = "Ben";
+    public static void main(String[] args)
+    {
+        // int age is a local variable
+        int age = 23;
+
+        System.out.println("My name is " + name + ", I am " + age + " years old");
+    }
+}
