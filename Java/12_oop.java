f84c347c0577a0bb82485e25234f35d019ebeadb
bcheong10 <bcheong10@gmail.com>
2022-12-23 17:51:55 +0800

    new lessons

--- Java/12_oop.java
+++ Java/12_oop.java
@@ -1,18 +1,38 @@
+import java.util.ArrayList;
+
 class oop {
     public static void main(String[] args) {
         
         // Declaring object
-        User user_1 = new User();
+        // User() is known as a constructor
+        User user_1 = new User("Ben", "Gold");
+        User user_2 = new User();
+
+        // Creating a list of objects
+        ArrayList<User> users = new ArrayList<User>();
+
+        // Add users object into arraylist
+        users.add(user_1);
+        users.add(user_2);
+
+        users.add(new User("Jon", "Silver"));
 
         // This set method cannot be used as attribute is set to private
         // user_1.name = "Ben";
         // user_1.membership = "Gold";
 
         // Using method getters & setters 
-        user_1.set_name("Sally");
-        user_1.set_membership(User.Membership.Gold);
+        user_2.set_name("Sally");
+        user_2.set_membership(User.Membership.Bronze);
         
-        System.out.println(user_1.get_name());
-        System.out.println(user_1.get_membership());
+        System.out.printf("Name: %s, Membership: %s\n", user_1.get_name(), user_1.get_membership());
+        System.out.printf("Name: %s, Membership: %s\n", user_2.get_name(), user_2.get_membership());
+        System.out.println(user_1);
+
+        // Print all users in arraylist
+        for (User user: users) {
+            System.out.println(user);
+        }
+    
     }
 }
--- Java/User.java
+++ Java/User.java
@@ -1,9 +1,33 @@
+import java.text.Format;
+
 public class User {
 
     // Set attributes as private
     private String name;
     private String membership;
 
+    // Creating a Constructor
+    public User(String name, String membership) {
+        this.name = name;
+        this.membership = membership;
+
+        // Or we can invoke the getter and setter methods:
+        // set_name(name)
+        // set_membership(membership)
+    }
+
+    // Creating a default constructor to enable default calling of object
+    public User() {
+
+    }
+    
+    // Method overriding 
+    // Returns this function when printing the object variable
+    public String toString() {
+        return "Name: " + this.name + "\n" + "Membership: " + this.membership;
+    }
+
+    // ---------------------------------------- METHODS -------------------------------------------------------------
     // Method #1 (or Functions in python) (Setters)
     void set_name(String name) {
         this.name = name;
@@ -24,6 +48,11 @@ public class User {
     void set_membership(Membership membership) {
         // .name() returns the name of the enums constants
         this.membership = membership.name();
+    }
+
+    // Set membership method (String)
+    void set_membership(String membership) {
+        this.membership = membership;
     }
 
     // Get membership method
